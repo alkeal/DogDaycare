bbdfc0f8ed3f90d5558a33892a10f0f3f63d4c5b
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-09-20 16:51:08 +0200

    Host fixat

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 
 import "./styles.css"
+import React, {useState} from 'react'
 
 export default function App() {
 
+      
 return "Hej"
+
+
+
+
+
+
+}
+
+
+function Navbar(){
+
+<>
+<nav className="navbar">
+<div className="navbar-container">
+
+<Link to="/" className="navbar-logo">
+
+<FontAwesomeIcon icon="fa-solid fa-shield-dog" />
+
+</Link>
+
+
+
+
+</div>
+
+
+
+
+</nav>
+
+
+
+</>
 
 
 }
