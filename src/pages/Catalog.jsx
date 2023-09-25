12f59577522b1e0dbbe6bdd70afb2b1a969a7e19
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-09-25 16:51:49 +0200

    API start

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+const apiUrl = 'https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683'
 
+const button = document.querySelector('#fetch');
 
 function Catalog() {
   return (
     <div className="catalog">Catalog1</div>
     
   )
+  
 }
+
+
+button.addEventListener('click', async e =>{
+
+const respone = await fetch(apiUrl);
+
+const dogData = await respone.json();
+
+const dogs = dogData.results;
+
+
+
+
+})
 
 export default Catalog
