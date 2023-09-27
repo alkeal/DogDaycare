81e7366a7e0a069d960bb220250042e9723b9a2c
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-09-27 22:17:22 +0200

    API test

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,28 +1,36 @@
 import React from 'react'
+import { useState, useEffect} from "react";
 const apiUrl = 'https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683'
 
-const button = document.querySelector('#fetch');
+const [dogs, setDogs] = useState(0)
+
 
 function Catalog() {
+
+  useEffect(() => {
+
+  const fetchData = async () => {
+
+    const result = await fetch(apiUrl)
+      result.json().then(json => {
+        setDogs(json.current.name)
+      }
+
+      )
+  }
+
+ fetchData();
+
+  },[] )
+
   return (
-    <div className="catalog">Catalog1</div>
+    <div className="catalog">Katalog</div>
     
   )
   
 }
 
 
-button.addEventListener('click', async e =>{
 
-const respone = await fetch(apiUrl);
-
-const dogData = await respone.json();
-
-const dogs = dogData.results;
-
-
-
-
-})
 
 export default Catalog
