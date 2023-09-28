443275cca008e78453af3c4fddc18ef79c9a398b
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-09-28 03:44:57 +0200

    APi

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,42 +1,64 @@
 import React from 'react'
 import { useState, useEffect} from "react";
+import "../pages/Catalog.css"
 
+const apiUrl = "https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683"
 
+ function Catalog () {
 
-function Catalog() {
-
-  function loadCatalog(){
-
-   fetch("https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683")
-     .then((response) => response.json())
-     .then((data) => {
-      setCatalog([...catalogs, data])
-     }
-
-  )}
-  const [catalogs, setCatalog] = useState([]);
-  useEffect(() =>  {
-      loadCatalog();
-  }, []);
-
-
-  return (
-    <div>
-   <ul>
-    {catalogs.map((record) => {
-       return <li key={record.key}>{record.lastName}</li>
-
-        } 
-      )}
-    
-   </ul>
- </div>
-    
-  )
   
+ const [userData, setUserData] = useState({});
+
+ useEffect(() => {
+
+  getDogUserData();
+
+ }, []);
+
+ const getDogUserData = async () => {
+   
+  const response = await fetch(apiUrl);
+  const jsonData = await response.json();
+  setUserData(jsonData);
+
+
+
+ };
+
+ return (
+  
+  <div className="Catalog">
+      <header className="Header-Catalog">
+
+        <h2>Hundinfo</h2>
+      </header>
+         <div className="dogUser-container">
+
+         <h5 className="dog-data">{userData.name}</h5>
+         <h5 className="dog-data">{userData.sex}</h5>
+         <h5 className="dog-data">{userData.breed}</h5>
+         <h5 className="dog-data">{userData.img}</h5>
+         <h5 className="dog-data">{userData.present}</h5>
+         <h5 className="dog-data">{userData.age}</h5>
+         <h5 className="dog-data">{userData.chipNumber}</h5>
+         <h5 className="dog-data">{userData.owner}</h5>
+
+
+         </div>
+
+
+
+  </div>
+
+
+
+ );
+ 
+
+
 }
 
 
 
 
-export default Catalog
+export default Catalog;
