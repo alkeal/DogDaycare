603375f446e8737eb67946c0efecef0fa6485033
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-10-02 14:44:43 +0200

    info2

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../pages/Info.css"
+import {Link} from "react-router-dom";
+import BackgroundImage from  "../assets/dogbackground.jpeg"
 
 
 function Info() {
@@ -12,13 +14,19 @@ function Info() {
 
   return (
 
+
+    <div className="info" style={{backgroundImage: `url(${BackgroundImage})`}}>
+
     <div className="info-headertext">Vi är ett hunddagis i Stockholm
     
     
-      <h2>Hej</h2>
+      <h2 className="info-small-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec blandit purus, non congue quam. Nulla vel libero eget sapien auctor bibendum. Integer sollicitudin ipsum eget nisi semper imperdiet. Sed posuere nibh vitae commodo vestibulum. Nunc ultricies, erat vitae finibus interdum, turpis enim bibendum lectus, ut ullamcorper arcu felis quis arcu. Morbi maximus enim a felis fermentum ultricies. Vivamus in dictum nunc. Donec vel ultricies libero. Vestibulum sed molestie orci. Quisque ut justo eget metus dignissim laoreet id nec leo. Quisque maximus, nisl at dictum pretium, tortor lorem mollis augue, id consequat dui urna nec nibh. Mauris fringilla, massa sit amet aliquet pretium, lorem dui elementum neque, id porta tortor magna eu ex.
+
+
+</h2>
     
     </div>
-
+   </div>
   )
 }
 
